6a4b36eae7bdc5329feaabe425aae8783210be15
Nooa Saareks <nsaareks@gmail.com>
2022-04-03 16:01:49 +0300

    Add shema.sql file

---
+++ schema.sql
@@ -0,0 +1,2 @@
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
+CREATE TABLE recipes (id SERIAL PRIMARY KEY, name TEXT, category TEXT, recipe TEXT, time TEXT, price TEXT);
