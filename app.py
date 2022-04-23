648227fdfb23dcc5ec57998ca278414d99b60d99
Nooa Saareks <nsaareks@gmail.com>
2022-04-23 16:01:10 +0300

    Bug fixes in app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 from werkzeug.security import check_password_hash, generate_password_hash
-from filldata import filldatapsql
+
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
